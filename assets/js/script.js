35f96c5f7dd3b7d7e0cddbba047f32b82e767c37
fayinana <fekedeananiya@gmail.com>
2024-07-02 22:04:19 +0300

    a new stractural update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,24 @@
 const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
 const links = document.querySelector(".links");
 
 menu.addEventListener("click", () => {
-  links.classList.toggle("hide-menu");
+  links.classList.toggle("show-links");
+});
+close.addEventListener("click", () => {
+  links.classList.toggle("show-links");
 });
 
-const dateDOM = document.querySelector(".date");
+const width = document.querySelector("body").clientWidth;
+window.addEventListener("change", () => {
+  if (width > 768) {
+    links.classList.remove("show-links");
+  }
+
+  console.log("change");
+});
+
+const dateDOM = document.querySelector("#date");
 const date = new Date();
 
 dateDOM.innerHTML = date.getFullYear();
-
-const navbar = document.getElementById("nav");
-window.addEventListener("scroll", function () {
-  const scrollHeight = window.pageYOffset;
-  const navHeight = navbar.getBoundingClientRect().height;
-  if (scrollHeight > navHeight) {
-    navbar.classList.add("fixed-nav");
-  } else {
-    navbar.classList.remove("fixed-nav");
-  }
-});
-
-const linksContainer = document.querySelector(".links-container");
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    scrollLinks.forEach((l) => {
-      l.classList.remove("active");
-    });
-    link.classList.add("active");
-
-    const id = e.currentTarget.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    const navHeight = navbar.getBoundingClientRect().height;
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    const fixedNav = navbar.classList.contains("fixed-nav");
-    let position = element.offsetTop - navHeight;
-
-    if (!fixedNav) {
-      position = position - navHeight;
-    }
-    if (navHeight > 82) {
-      position = position + containerHeight;
-    }
-
-    window.scrollTo({
-      left: 0,
-      top: position,
-    });
-    linksContainer.style.height = 0;
-  });
-});
